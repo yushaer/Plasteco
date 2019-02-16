0ca9d7172c8e1f0620d6a6fd4d41a11f02e24d45
apollofortytwo <32778577+BryanHuynh@users.noreply.github.com>
2019-02-16 16:15:58 -0700

    Merge branch 'master' of https://github.com/yushaer/Plasteco into objectdetail
    # Conflicts:
    #	app/src/main/java/com/plastic/plastico/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/plastic/plastico/MainActivity.java
+++ app/src/main/java/com/plastic/plastico/MainActivity.java
@@ -1,11 +1,13 @@
 package com.plastic.plastico;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button cameraButton = (Button) findViewById(R.id.objectDetails);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ObjectDetails.class));
             }
         });
     }
